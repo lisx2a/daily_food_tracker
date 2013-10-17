e393f159b260baf54102782674679f26d41fb711
Lissa <lissa.aguilar@gmail.com>
2013-10-17 00:22:10 -0400

    added trackerSpec.js

---
+++ spec/trackerSpec.js
@@ -0,0 +1,15 @@
+describe("add new entry", function(){
+	it("adds a new entry",function()
+});
+
+describe("retrieve entry", function(){
+	it("gets a saved entry and displays data")
+});
+
+describe("add mood", function(){
+	it("required adds a mood icon to the data entry")
+});
+
+describe("enter reaction", function(){
+	it("enters reaction to foodi, makes required")
+});
